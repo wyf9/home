595d17239d8566858ce2089d63f2590d2d653477
wyf9 <wyf9@wyf9.top>
2024-08-24 14:31:37 +0800

    change homeworks

---
+++ note/cpp/output.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#coding:utf-8
+
+while True:
+    while True:
+        print('Modes: i (input) / o (output)')
+        mode = imput('- Mode: ')
+        # match mode:
+            # case 'i' | 
